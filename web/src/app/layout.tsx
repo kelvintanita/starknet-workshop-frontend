c5ff1ef6db72cf95e1cbd036f6fa28a6c7f28ca7
kelvin.k.tanita <kelvin.k.tanita@kroton.com.br>
2024-09-09 02:43:31 -0300

    feat: update sepolia contract

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Basecamp IX",
+  title: "Basecamp Brasil",
   description: "Frontend Workshop",
 };
 
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
-'use client';
-import Head from 'next/head';
-import dynamic from 'next/dynamic';
+"use client";
+import Head from "next/head";
+import dynamic from "next/dynamic";
 import {
   useBlockNumber,
   useAccount,
@@ -12,43 +12,81 @@ import {
   useWaitForTransaction,
 } from "@starknet-react/core";
 import { BlockNumber } from "starknet";
-import contractAbi from "../abis/abi.json";
-import { useState, useMemo } from 'react';
+import contractAbi from "../abis/abi_erc20.json";
+import { useState, useMemo } from "react";
 
-const WalletBar = dynamic(() => import('../components/WalletBar'), { ssr: false })
+const WalletBar = dynamic(() => import("../components/WalletBar"), {
+  ssr: false,
+});
+
 const Page: React.FC = () => {
-
   // Step 1 --> Read the latest block -- Start
-  const { data: blockNumberData, isLoading: blockNumberIsLoading, isError: blockNumberIsError } = useBlockNumber({
-    blockIdentifier: 'latest' as BlockNumber
+  const {
+    data: blockNumberData,
+    isLoading: blockNumberIsLoading,
+    isError: blockNumberIsError,
+  } = useBlockNumber({
+    blockIdentifier: "latest" as BlockNumber,
   });
-  const workshopEnds = 68224;
+
+  const workshopEnds = 704701;
+
   // Step 1 --> Read the latest block -- End
 
   // Step 2 --> Read your balance -- Start
   const { address: userAddress } = useAccount();
-  const { isLoading: balanceIsLoading, isError: balanceIsError, error: balanceError, data: balanceData } = useBalance({
+  const {
+    isLoading: balanceIsLoading,
+    isError: balanceIsError,
+    error: balanceError,
+    data: balanceData,
+  } = useBalance({
     address: userAddress,
-    watch: true
+    watch: true,
   });
   // Step 2 --> Read your balance -- End
 
   // Step 3 --> Read from a contract -- Start
-  const contractAddress = "0x1e599a44cb196b0fa8e6af83457301e89adfb32e158fa3044cc57c4a823e877";
-  const { data: readData, refetch: dataRefetch, isError: readIsError, isLoading: readIsLoading, error: readError } = useContractRead({
-    functionName: "get_balance",
-    args: [],
+  const contractAddress = 
+    "0x019eaa190f47d499c6ff6e9688f58351bde9b60acf57fb687e7da94c496f28c6";
+
+  // Read balance_of
+  const {
+    data: readData,
+    refetch: dataRefetch,
+    isError: readIsError,
+    isLoading: readIsLoading,
+    error: readError,
+  } = useContractRead({
+    functionName: "balance_of",
+    args: [userAddress ? userAddress : "0x0"],
     abi: contractAbi,
     address: contractAddress,
     watch: true,
   });
+
+  // Read total_supply
+  const {
+    data: readTotalSupply,
+    refetch: totalSupplyRefetch,
+    isError: totalSupplyIsError,
+    isLoading: totalSupplyIsLoading,
+    error: totalSupplyError,
+  } = useContractRead({
+    functionName: "total_supply",
+    abi: contractAbi,
+    address: contractAddress,
+    watch: true,
+  });
+
   // Step 3 --> Read from a contract -- End
 
   // Step 4 --> Write to a contract -- Start
+
   const [amount, setAmount] = useState(0);
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Form submitted with amount ", amount);
+    /* console.log("Form submitted with amount ", amount); */
     // TO DO: Implement Starknet logic here
     writeAsync();
   };
@@ -56,10 +94,15 @@ const Page: React.FC = () => {
     abi: contractAbi,
     address: contractAddress,
   });
+
   const calls = useMemo(() => {
     if (!userAddress || !contract) return [];
-    return contract.populateTransaction["increase_balance"]!({ low: (amount ? amount : 0), high: 0 });
+    return contract.populateTransaction["mint"]!(userAddress, {
+      low: amount ? amount : 0,
+      high: 0,
+    });
   }, [contract, userAddress, amount]);
+
   const {
     writeAsync,
     data: writeData,
@@ -67,18 +110,38 @@ const Page: React.FC = () => {
   } = useContractWrite({
     calls,
   });
+
   const explorer = useExplorer();
-  const { isLoading: waitIsLoading, isError: waitIsError, error: waitError, data: waitData } = useWaitForTransaction({ hash: writeData?.transaction_hash, watch: true })
+
+  const {
+    isLoading: waitIsLoading,
+    isError: waitIsError,
+    error: waitError,
+    data: waitData,
+  } = useWaitForTransaction({ hash: writeData?.transaction_hash, watch: true });
+
   const LoadingState = ({ message }: { message: string }) => (
     <div className="flex items-center space-x-2">
       <div className="animate-spin">
-        <svg className="h-5 w-5 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+        <svg
+          className="h-5 w-5 text-gray-800"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
         </svg>
       </div>
       <span>{message}</span>
     </div>
   );
+
   const buttonContent = () => {
     if (writeIsPending) {
       return <LoadingState message="Send..." />;
@@ -98,10 +161,44 @@ const Page: React.FC = () => {
 
     return "Send";
   };
+
+  const [recipient, setRecipient] = useState<string>("");
+  const [transferAmount, setTransferAmount] = useState<number>(0);
+
+  const transferCalls = useMemo(() => {
+    if (!contract || !recipient || !transferAmount) return [];
+    return contract.populateTransaction["transfer"](recipient, {
+      low: transferAmount,
+      high: 0,
+    });
+  }, [contract, recipient, transferAmount]);
+
+  const {
+    writeAsync: transferWriteAsync,
+    data: transferWriteData,
+    isPending: transferIsPending,
+  } = useContractWrite({
+    calls: transferCalls,
+  });
+
+  const handleTransferSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    try {
+      if (transferWriteAsync) {
+        await transferWriteAsync();
+        console.log("Transfer submitted");
+      }
+    } catch (error) {
+      console.error("Error during transfer: ", error);
+    }
+  };
+
   // Step 4 --> Write to a contract -- End
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center pt-20 pb-20">
       <Head>
         <title>Frontend Workshop</title>
       </Head>
@@ -112,47 +209,36 @@ const Page: React.FC = () => {
       {/* Step 1 --> Read the latest block -- Start */}
       {!blockNumberIsLoading && !blockNumberIsError && (
         <div
-          className={`p-4 w-full max-w-md m-4 border-black border ${blockNumberData! < workshopEnds ? "bg-green-500" : "bg-red-500"}`}
+          className={`p-4 w-full max-w-md m-4 border-black border ${
+            blockNumberData! < workshopEnds ? "bg-green-500" : "bg-red-500"
+          }`}
         >
           <h3 className="text-2xl font-bold mb-2">Read the Blockchain</h3>
           <p>Current Block Number: {blockNumberData}</p>
-          {blockNumberData! < workshopEnds ? "We're live on Workshop" : "Workshop has ended"}
+          {blockNumberData! < workshopEnds
+            ? "We're live on Workshop"
+            : "Workshop has ended"}
         </div>
       )}
-      {/* <div
-        className={`p-4 w-full max-w-md m-4 border-black border bg-white`}
-      >
-        <h3 className="text-2xl font-bold mb-2">Read the Blockchain</h3>
-        <p>Current Block Number: xyz</p>
-        Are we live?
-      </div> */}
+
       {/* Step 1 --> Read the latest block -- End */}
 
       {/* Step 2 --> Read your balance -- Start */}
       {!balanceIsLoading && !balanceIsError && (
-        <div
-          className={`p-4 w-full max-w-md m-4 bg-white border-black border`}
-        >
+        <div className={`p-4 w-full max-w-md m-4 bg-white border-black border`}>
           <h3 className="text-2xl font-bold mb-2">Read your Balance</h3>
           <p>Symbol: {balanceData?.symbol}</p>
           <p>Balance: {Number(balanceData?.formatted).toFixed(4)}</p>
         </div>
       )}
-      {/* <div
-        className={`p-4 w-full max-w-md m-4 bg-white border-black border`}
-      >
-        <h3 className="text-2xl font-bold mb-2">Read your Balance</h3>
-        <p>Symbol: Ticker</p>
-        <p>Balance: xyz</p>
-      </div> */}
+
       {/* Step 2 --> Read your balance -- End */}
 
       {/* Step 3 --> Read from a contract -- Start */}
-      <div
-        className={`p-4 w-full max-w-md m-4 bg-white border-black border`}
-      >
+      <div className={`p-4 w-full max-w-md m-4 bg-white border-black border`}>
         <h3 className="text-2xl font-bold mb-2">Read your Contract</h3>
         <p>Balance: {readData?.toString()}</p>
+        <p>Total supply: {readTotalSupply?.toString()}</p>{" "}
         <div className="flex justify-center pt-4">
           <button
             onClick={() => dataRefetch()}
@@ -162,25 +248,17 @@ const Page: React.FC = () => {
           </button>
         </div>
       </div>
-      {/* <div
-        className={`p-4 w-full max-w-md m-4 bg-white border-black border`}
-      >
-        <h3 className="text-2xl font-bold mb-2">Read your Contract</h3>
-        <p>Balance: xyz</p>
-        <div className="flex justify-center pt-4">
-          <button
-            onClick={() => console.log("Refresh")}
-            className={`border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500`}
-          >
-            Refresh
-          </button>
-        </div>
-      </div> */}
+
       {/* Step 3 --> Read from a contract -- End */}
 
       {/* Step 4 --> Write to a contract -- Start */}
-      <form onSubmit={handleSubmit} className="bg-white p-4 w-full max-w-md m-4 border-black border">
-        <h3 className="text-2xl font-bold mb-2">Write to a Contract</h3>
+
+      {/* Mint ERC20 */}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-4 w-full max-w-md m-4 border-black border"
+      >
+        <h3 className="text-2xl font-bold mb-2">Mint ERC20</h3>
         <label
           htmlFor="amount"
           className="block text-sm font-medium leading-6 text-gray-900"
@@ -199,49 +277,87 @@ const Page: React.FC = () => {
             href={explorer.transaction(writeData?.transaction_hash)}
             target="_blank"
             className="text-blue-500 hover:text-blue-700 underline"
-            rel="noreferrer">Check TX on {explorer.name}</a>
+            rel="noreferrer"
+          >
+            Check TX on {explorer.name}
+          </a>
         )}
         <div className="flex justify-center pt-4">
           <button
             type="submit"
-            className={`border border-black text-black font-regular py-2 px-4 ${userAddress ? "bg-yellow-300 hover:bg-yellow-500" : "bg-white"} `}
+            className={`border border-black text-black font-regular py-2 px-4 ${
+              userAddress ? "bg-yellow-300 hover:bg-yellow-500" : "bg-white"
+            } `}
             disabled={!userAddress}
           >
             {buttonContent()}
           </button>
         </div>
       </form>
-      {/* <form onSubmit={handleSubmit} className="bg-white p-4 w-full max-w-md m-4 border-black border">
-        <h3 className="text-2xl font-bold mb-2">Write to a Contract</h3>
+
+      {/* Transfer ERC20 Tokens */}
+      <form
+        onSubmit={handleTransferSubmit}
+        className="bg-white p-4 w-full max-w-md m-4 border-black border"
+      >
+        <h3 className="text-2xl font-bold mb-2">Transfer ERC20 Tokens</h3>
         <label
-          htmlFor="amount"
+          htmlFor="recipient"
           className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Recipient Address:
+        </label>
+        <input
+          type="text"
+          id="recipient"
+          value={recipient}
+          onChange={(event) => setRecipient(event.target.value)}
+          className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300"
+          placeholder="Recipient Address"
+        />
+
+        <label
+          htmlFor="transferAmount"
+          className="block text-sm font-medium leading-6 text-gray-900 mt-2"
         >
           Amount:
         </label>
         <input
           type="number"
-          id="amount"
-          value={amount}
-          onChange={(event) => setAmount(event.target.valueAsNumber)}
-          className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300 black-border-p"
+          id="transferAmount"
+          value={transferAmount}
+          onChange={(event) => setTransferAmount(event.target.valueAsNumber)}
+          className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300"
+          placeholder="Amount"
         />
-        <a
-          href={"https://x.com/0xNestor"}
-          target="_blank"
-          className="text-blue-500 hover:text-blue-700 underline"
-          rel="noreferrer">Check TX on an explorer</a>
+
         <div className="flex justify-center pt-4">
           <button
             type="submit"
-            className={`border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500`}
+            className={`border border-black text-black font-regular py-2 px-4 ${
+              recipient && transferAmount
+                ? "bg-yellow-300 hover:bg-yellow-500"
+                : "bg-gray-300 cursor-not-allowed"
+            }`}
+            disabled={!recipient || !transferAmount || transferIsPending}
           >
-            Send
+            {buttonContent()}
           </button>
         </div>
-      </form> */}
-      {/* Step 4 --> Write to a contract -- End */}
 
+        {transferWriteData?.transaction_hash && (
+          <a
+            href={explorer.transaction(transferWriteData?.transaction_hash)}
+            target="_blank"
+            className="text-blue-500 hover:text-blue-700 underline"
+            rel="noreferrer"
+          >
+            Check TX on {explorer.name}
+          </a>
+        )}
+      </form>
+
+      {/* Step 4 --> Write to a contract -- End */}
     </div>
   );
 };
